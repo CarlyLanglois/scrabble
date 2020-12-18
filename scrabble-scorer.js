23862432ab21c2ccbdace608ebc8f097eea1c2b7
CarlyLanglois <cj.langlois.cl@gmail.com>
2020-12-18 11:22:01 -0600

    Initial commit

---
+++ scrabble-scorer.js
@@ -0,0 +1,45 @@
+// inspired by https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a3cca9548cebe5975d7ebca6a85
+
+const input = require("readline-sync");
+
+const oldPointStructure = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+};
+
+function initialPrompt() {};
+
+function transform() {};
+
+let simpleScore;
+
+let vowelBonusScore;
+
+let scrabbleScore;
+
+const scoringAlgorithms = [];
+
+let newPointStructure;
+
+function runProgram() {
+ 
+	
+}
+
+module.exports = {
+   initialPrompt: initialPrompt,
+   transform: transform,
+   oldPointStructure: oldPointStructure,
+   simpleScore: simpleScore,
+   vowelBonusScore: vowelBonusScore,
+   scrabbleScore: scrabbleScore,
+	scoringAlgorithms: scoringAlgorithms,
+	newPointStructure: newPointStructure,
+	runProgram: runProgram
+};
+
